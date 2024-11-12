7af71d4ad1a05a213ea6b9eeca785a34ae6afd5b
DrueWolf <dwolf0305@gmail.com>
2024-11-11 20:54:25 -0500

    part mewssage and board render

--- exercises.py
+++ exercises.py
@@ -9,3 +9,26 @@ class Game:
             'a2': None, 'b2': None, 'c2': None,
             'a3': None, 'b3': None, 'c3': None,
         }
+
+    def print_board(self):
+        b = self.board
+        print(f"""
+              A   B   C
+          1)  {b['a1'] or ' '} | {b['b1'] or ' '} | {b['c1'] or ' '}
+              ----------
+          2)  {b['a2'] or ' '} | {b['b2'] or ' '} | {b['c2'] or ' '}
+              ----------
+          3)  {b['a3'] or ' '} | {b['b3'] or ' '} | {b['c3'] or ' '}
+        """)
+
+    def print_message(self):
+        if self.tie:
+            print("It's a tie!")
+        elif self.winner:
+            print(f"{self.winner} wins the game!")
+        else:
+            print(f"It's player {self.turn}'s turn!")
+
+    def render(self):
+        self.print_board()
+        self.print_message()
