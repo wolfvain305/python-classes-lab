ff23bb1e214c0fa74b499fcea7d35ee657650ad7
DrueWolf <dwolf0305@gmail.com>
2024-11-17 20:50:50 -0500

    set the game state

--- exercises.py
+++ exercises.py
@@ -3,41 +3,22 @@ class Game:
         self.turn = 'X'
         self.tie = False
         self.winner = None
-        
         self.board = {
             'a1': None, 'b1': None, 'c1': None,
             'a2': None, 'b2': None, 'c2': None,
             'a3': None, 'b3': None, 'c3': None,
         }
 
-    def print_board(self):
-        b = self.board
-        print(f"""
-            A   B   C
-        1)  {b['a1'] or ' '} | {b['b1'] or ' '} | {b['c1'] or ' '}
-            ----------
-        2)  {b['a2'] or ' '} | {b['b2'] or ' '} | {b['c2'] or ' '}
-            ----------
-        3)  {b['a3'] or ' '} | {b['b3'] or ' '} | {b['c3'] or ' '}
-        """)
-
-    def print_message(self):
-        if self.tie:
-            print("It's a tie!")
-        elif self.winner:
-            print(f"{self.winner} wins the game!")
-        else:
-            print(f"It's player {self.turn}'s turn!")
-
-    def render(self):
-        self.print_board()
-        self.print_message()
-
-    def get_move(self):
-        while true:
-            move = input("enter a valid move (example: A1):").lower()
-            if move in self.board and self.board[move] is None:
-                self.board[move] = self.turn
-                break
-            else:
-                print("Invalid move ! Please try again.")
+    def play_game(self):
+        print(" Welcome to Tic-Tac-Toe!")
+        while not self.winner and not self.tie:
+            self.render()
+            self.get_move()
+            self.check_for_winner()
+            self.check_for_tie()
+            self.switch_turn()
+        self.render()
+        if self.winner:
+            print(f"Congratulations! Player {self.winner} wins!")
+        elif self.tie:
+            print("it's a tie!")
