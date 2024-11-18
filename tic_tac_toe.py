f1d7d54729aca65cdd170359a260a206d9c255ab
DrueWolf <dwolf0305@gmail.com>
2024-11-17 21:32:21 -0500

    code commplete

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -56,8 +56,24 @@ class Game:
             else:
                 print("Invalid move! Please try again!")
 
-    def
-
+    def check_for_winner(self):
+        winning_combinations = [
+            ['a1', 'b1', 'c1'],  
+            ['a2', 'b2', 'c2'],  
+            ['a3', 'b3', 'c3'],  
+            ['a1', 'a2', 'a3'],  
+            ['b1', 'b2', 'b3'],  
+            ['c1', 'c2', 'c3'],  
+            ['a1', 'b2', 'c3'],  
+            ['c1', 'b2', 'a3'],  
+        ]
+        for combo in winning_combinations:
+            if (
+                self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]] == self.turn
+            ):
+                self.winner = self.turn
+                return
+            
     def check_for_tie(self):
         if all(value is not None for value in self.board.values()) and not self.winner:
             self.tie = True
