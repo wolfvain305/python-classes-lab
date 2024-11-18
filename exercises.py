0e489ccfea754efb69d4cb2f32db5542faa24548
DrueWolf <dwolf0305@gmail.com>
2024-11-17 20:57:57 -0500

    player movement

--- exercises.py
+++ exercises.py
@@ -49,3 +49,14 @@ class Game:
     def render(self):
         self.print_board()
         self.print_message()
+
+    def get_move(self):
+        while True:
+            move = input(" Ener a valid move (example: A1):").lower()
+            if move in self.board and self.board[move] is None:
+                self.board[move] = self.turn
+                break
+            else:
+                print("Invalid move! Please try again!")
+
+    
